cba768e492e77faef668b94e7c9a99bed6fd7557
Nathalia <nathaliapadovani.contato@gmail.com>
2025-10-26 20:46:52 -0300

    fix: ajustar base vazia para corrigir caminhos absolutos no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./", // 👈 caminho relativo (ESSENCIAL para GitHub Pages)
+  base: "", // 👈 caminho relativo (ESSENCIAL para GitHub Pages)
   server: {
     host: "::",
     port: 8080,
