886690799c9c947f15f9906afbf83b2f015457a8
Nathalia <113705196+nathaliapadovani@users.noreply.github.com>
2025-11-17 18:53:16 -0300

    Update base path in Vite configuration
    Changed base path from relative to absolute.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./", // 👈 caminho relativo (funciona em qualquer subpasta)
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
